ae72c5a0a721446147d7edd4e6111f846c5233b4
jasonnn <jasonmarkham@gmail.com>
2017-02-20 19:40:52 -0800

    initial implementation of changes described in chief delphi

--- Truffles/src/Subsystems/GearCatch.cpp
+++ Truffles/src/Subsystems/GearCatch.cpp
@@ -5,14 +5,18 @@
 #include "GearCatch.h"
 #include "../Commands/GearCatchCommands.h"
 #include "../RobotMap.h"
+/**
+ * see
+ https://www.chiefdelphi.com/forums/showthread.php?t=154631&highlight=linear+actuator+servo
+ *
 
+ */
 GearCatch::GearCatch() : Subsystem("Gear Catch") {
   actuator1 = RobotMap::gearCatchActuator1;
   actuator2 = RobotMap::gearCatchActuator2;
-
-  actuator1->SetBounds(2.0, 0, 0, 0, 1.0);
-  // actuator1->SetPeriodMultiplier(PWM::kPeriodMultiplier_1X);
-  actuator2->SetBounds(2.0, 0, 0, 0, 1.0);
+//Moved to LinearActuator
+//  actuator1->SetBounds(2.0, 0, 0, 0, 1.0);
+//  actuator2->SetBounds(2.0, 0, 0, 0, 1.0);
 }
 
 void GearCatch::InitDefaultCommand() {
@@ -23,18 +27,27 @@ void GearCatch::moveOut() {
   setPosition(0.615);
 }
 
+/*
+ * from the above link:
+ * ... -The basic set function does not seem to work for whatever reason.
+ * Using setSpeed(1) and setSpeed(-1) properly calls setMaxPWM and setMinPWM
+ * wheres set(1) and set(0) do not properly call them.
+ */
 void GearCatch::setPosition(double position) {
-  // Servo.set expects input in [0,1]
-  actuator1->Set(position);
-  actuator2->Set(position);
+    //Moved to LinearActuator
+//  double scaledPosition = 2 * position - 1;
+//
+//  actuator1->SetSpeed(scaledPosition);
+//  actuator2->SetSpeed(scaledPosition);
+  //    // Servo.set expects input in [0,1]
+    actuator1->Set(position);
+    actuator2->Set(position);
 }
 
-// void GearCatch::moveVertical() {
-//    moveTo(0.1);
-//
-//}
-
 void GearCatch::moveIn() {
+  setPosition(0);
+}
+void GearCatch::chill() {
   // TODO does this work better than SetPosition(0) ?
   actuator1->SetOffline();
   actuator2->SetOffline();
@@ -53,6 +66,7 @@ void GearCatch::moveTo(GearCatch::Position p) {
       setPosition(0.0);
       break;
     case CHILL:
+        chill();
       break;
   }
 }
