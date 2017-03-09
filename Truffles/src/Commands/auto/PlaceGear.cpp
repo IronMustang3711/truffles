44d9a5ac5ca40bbbbc980164ed4ef08d8811bca0
jasonnn <jasonmarkham@gmail.com>
2017-03-08 17:36:06 -0800

    add fake parameters to drive straight commands to get it to compile

--- Truffles/src/Commands/auto/PlaceGear.cpp
+++ Truffles/src/Commands/auto/PlaceGear.cpp
@@ -12,26 +12,26 @@ PlaceGear::PlaceGear(int mode) : CommandGroup() {
   switch (mode) {
     case 0:
       // Robot starting on left side (in front of driver station 1)
-      AddSequential(new DriveStraight());
-      AddSequential(new RotateCommand());
+      AddSequential(new DriveStraight(1));
+      AddSequential(new RotateCommand(1));
       AddSequential(new PixyRotate());
       AddSequential(new PixyStrafe());
-      AddSequential(new DriveStraight());
+      AddSequential(new DriveStraight(1));
       break;
     case 1:
       // Robot starting in the middle (in front of driver station 2)
-      AddSequential(new DriveStraight());
+      AddSequential(new DriveStraight(1));
       AddSequential(new PixyRotate());
       AddSequential(new PixyStrafe());
-      AddSequential(new DriveStraight());
+      AddSequential(new DriveStraight(1));
       break;
     case 2:
       // Robot starting on the right side (in front of driver station 3)
-      AddSequential(new DriveStraight());
-      AddSequential(new RotateCommand());
+      AddSequential(new DriveStraight(1));
+      AddSequential(new RotateCommand(1));
       AddSequential(new PixyRotate());
       AddSequential(new PixyStrafe());
-      AddSequential(new DriveStraight());
+      AddSequential(new DriveStraight(1));
       break;
   }
   AddSequential(new TogglePixyLight());  // Turn Pixy light off again
