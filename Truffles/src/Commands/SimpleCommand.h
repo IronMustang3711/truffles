f263a0fd9d3ef9bfbcb9d346b733385e6ebf05bb
jasonnn <jasonmarkham@gmail.com>
2017-03-16 23:30:26 -0700

    Remove handling of Iterrupted in SimpleCommand

--- Truffles/src/Commands/SimpleCommand.h
+++ Truffles/src/Commands/SimpleCommand.h
@@ -5,15 +5,15 @@
 #ifndef TRUFFLES_SIMPLECOMMAND_H
 #define TRUFFLES_SIMPLECOMMAND_H
 #include <Commands/Command.h>
-
+//Used in Hexapus Commands
 class SimpleCommand: public Command {
 public:
 	SimpleCommand(const std::string& name);
 
 	SimpleCommand(const std::string& name, double timeout);
 
-protected:
-	void Interrupted() override;
+//protected:
+//	void Interrupted() override;
 
 protected:
 	bool IsFinished() override;
