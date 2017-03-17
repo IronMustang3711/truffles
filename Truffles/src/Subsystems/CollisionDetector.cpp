cadd72225bfcd43c48b452fe7761d01a2b0aa54a
jasonnn <jasonmarkham@gmail.com>
2017-03-17 10:15:42 -0700

    collision detector

--- Truffles/src/Subsystems/CollisionDetector.cpp
+++ Truffles/src/Subsystems/CollisionDetector.cpp
@@ -1,8 +1,39 @@
 #include "CollisionDetector.h"
 #include "../RobotMap.h"
+#include "SmartDashboard/SmartDashboard.h"
+#include "DriverStation.h"
+#include "Commands/Command.h"
+#include "../Robot.h"
+#include <string>
+using namespace frc;
+
+class DashboardCollisionNotifier: public frc::Command {
+public:
+	DashboardCollisionNotifier() :
+			Command("dashboard collision notifier") {
+		Requires(Robot::collisionDetector.get());
+	}
+	bool IsFinished() override {
+		return false;
+	}
+
+	void Execute() override {
+		SmartDashboard::PutBoolean("collision detected: ",
+				Robot::collisionDetector->impactDetected());
+		SmartDashboard::PutNumber("max jerk(x)",
+				Robot::collisionDetector->maxJerkX);
+		SmartDashboard::PutNumber("max jerk(y)",
+				Robot::collisionDetector->maxJerkY);
+	}
+
+};
+
+void CollisionDetector::InitDefaultCommand() {
+	SetDefaultCommand(new DashboardCollisionNotifier());
+}
 
 CollisionDetector::CollisionDetector() :
-		Subsystem("ExampleSubsystem"), ahrs(RobotMap::ahrs.get()) {
+		Subsystem("Collision Detector"), ahrs(RobotMap::ahrs.get()) {
 	start();
 
 }
@@ -12,19 +43,19 @@ void CollisionDetector::resetDetector() {
 	last_world_linear_accel_y = 0;
 	collisionDetected = false;
 }
-static const double COLLISION_THRESHOLD_DELTA_G = 0.5;
+static const double COLLISION_THRESHOLD_DELTA_G = 0.2;
 
 bool CollisionDetector::impactDetected() {
 	return collisionDetected.load();
 }
 
 void CollisionDetector::stop() {
-	notifier.StartPeriodic(0.01);
+	notifier.Stop();
 }
 
 void CollisionDetector::start() {
 	resetDetector();
-	notifier.Stop();
+	notifier.StartPeriodic(0.01);
 }
 
 void CollisionDetector::update() {
@@ -34,13 +65,21 @@ void CollisionDetector::update() {
 	double curr_world_linear_accel_y = ahrs->GetWorldLinearAccelY();
 	double currentJerkY = curr_world_linear_accel_y - last_world_linear_accel_y;
 	last_world_linear_accel_y = curr_world_linear_accel_y;
+	if (fabs(currentJerkX) > maxJerkX) {
+		maxJerkX = fabs(currentJerkX);
+		std::string msg;
+		printf("jerk x: %f", maxJerkX);
 
+	}
+	if (fabs(currentJerkY) > maxJerkY) {
+		maxJerkY = fabs(currentJerkY);
+		printf("jerk y: %f", maxJerkY);
+	}
 	if ((fabs(currentJerkX) > COLLISION_THRESHOLD_DELTA_G)
 			|| (fabs(currentJerkY) > COLLISION_THRESHOLD_DELTA_G)) {
 		collisionDetected = true;
+		DriverStation::ReportError("collision detected!");
 	}
-	SmartDashboard::PutBoolean("CollisionDetected", collisionDetected);
 
 }
-// Put methods for controlling this subsystem
-// here. Call these from Commands.
+
--- Truffles/src/Subsystems/CollisionDetector.h
+++ Truffles/src/Subsystems/CollisionDetector.h
@@ -6,12 +6,15 @@
 #include <atomic>
 #include "AHRS.h"
 class CollisionDetector : public Subsystem {
+
 private:
 	// It's desirable that everything possible under private except
 	// for methods that implement subsystem capabilities
 
 public:
 	CollisionDetector();
+
+	virtual void InitDefaultCommand() override;
 
 
 	void resetDetector();
@@ -23,12 +26,14 @@ public:
 
 
 
-private:
+//private:
     void update();
     Notifier notifier{&CollisionDetector::update,this};
     AHRS *ahrs;             // navX MXP
     double last_world_linear_accel_x = 0;
     double last_world_linear_accel_y = 0;
+    double maxJerkX = 0;
+    double maxJerkY = 0;
     std::atomic_bool collisionDetected{false};
 
 };
